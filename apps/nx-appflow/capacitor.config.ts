650d24e692ac031bae9531d140c00b8416540d12
Devin Shoemaker <devinshoe@gmail.com>
2023-01-30 15:42:32 -0600

    wip

---
+++ apps/nx-appflow/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'text-nx-appflow',
+  webDir: '../../dist/apps/nx-appflow',
+  bundledWebRuntime: false
+};
+
+export default config;
